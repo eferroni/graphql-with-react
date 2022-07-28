f02f77d54728b6e6254a69cfd274698f106ec911
eduardo-ferroni <eduard.ferroni@hp.com>
2022-07-28 17:34:56 -0300

    finish section 8

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { Link, hashHistory } from "react-router";
+import fetchSongs from "../queries/fetchSongs";
+import addSong from "../queries/addSong";
 
 class SongCreate extends Component {
   constructor(props) {
@@ -6,11 +10,24 @@ class SongCreate extends Component {
     this.state = { title: "" };
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+    this.props
+      .mutate({
+        variables: {
+          title: this.state.title,
+        },
+        refetchQueries: [{ query: fetchSongs }],
+      })
+      .then(() => hashHistory.push("/"));
+  }
+
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
           <input
             onChange={(event) => this.setState({ title: event.target.value })}
@@ -23,4 +40,4 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate;
+export default graphql(addSong)(SongCreate);
--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,35 +1,57 @@
-import React, { useEffect, useState } from "react";
-import gql from "graphql-tag";
+import React from "react";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import fetchSongs from "../queries/fetchSongs";
+import deleteSong from "../queries/deleteSong";
 
 const SongList = (props) => {
   const { error, loading, songs } = props.data;
+
+  const onSongDelete = (id) => {
+    props.mutate({ variables: { id } });
+  };
+
   if (loading) {
     return <div>loading...</div>;
   }
+
   if (error) {
     return <div>error: {error}</div>;
   }
+
   const renderSongs = () => {
-    return songs.map((song) => {
+    return songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          <div
+            style={{
+              display: "flex",
+              alignContent: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            {title}
+            <i
+              className="material-icons"
+              style={{ cursor: "pointer" }}
+              onClick={() => onSongDelete(id)}
+            >
+              delete
+            </i>
+          </div>
         </li>
       );
     });
   };
 
-  return <ul className="collection">{renderSongs()}</ul>;
+  return (
+    <div>
+      <ul className="collection">{renderSongs()}</ul>
+      <Link to="/songs/new" className="btn-floating btn-large blue right">
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
+  );
 };
 
-const query = gql`
-  {
-    songs {
-      id
-      title
-    }
-  }
-`;
-
-export default graphql(query)(SongList);
+export default graphql(deleteSong)(graphql(fetchSongs)(SongList));
