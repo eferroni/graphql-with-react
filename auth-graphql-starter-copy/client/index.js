aca42c46e3589bc010523338c0c6a4cecab6b9ae
eduardo-ferroni <eduard.ferroni@hp.com>
2022-08-08 14:10:57 -0300

    finish section 12

---
+++ auth-graphql-starter-copy/client/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+
+const client = new ApolloClient({
+  dataIdFromObject: (object) => object.id,
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
